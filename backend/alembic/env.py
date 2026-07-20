b402ff6bfe9de158ce1971d8589c4e8dea3ade10
Mattis <lefrancadammattis@gmail.com>
2026-07-20 23:02:10 +0200

    db update

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,7 +5,8 @@ from sqlalchemy import pool
 from app.core.database import Base
 from app.core.config import settings
 from alembic import context
-
+from app.modules.users.models import User
+from app.modules.finance.models import Account
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
