d0aedb5181285096d1aec7a3c9a0093dec84f054
Tisa_48 <130873934+Tisa40486@users.noreply.github.com>
2026-07-30 19:10:10 +0100

    done

--- ios/Auren/Auren/Services/APIClient.swift
+++ ios/Auren/Auren/Services/APIClient.swift
@@ -18,7 +18,8 @@ class APIClient {
                 .map { key, value in "\(key)=\(value)" }
                 .joined(separator: "&")
             request.httpBody = encoded.data(using: .utf8)
-        } else if let body = body {
+        }
+        else if let body = body {
             request.setValue("application/json", forHTTPHeaderField: "Content-Type")
             request.httpBody = try JSONEncoder().encode(body)
         }
@@ -30,5 +31,23 @@ class APIClient {
         }
         print("Body reçu: \(String(data: data, encoding: .utf8) ?? "nil")")
 
-        return try JSONDecoder().decode(T.self, from: data)
-    }}
+        let decoder = JSONDecoder()
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+
+        decoder.dateDecodingStrategy = .custom { decoder in
+            let container = try decoder.singleValueContainer()
+            let dateString = try container.decode(String.self)
+
+            let formatter = ISO8601DateFormatter()
+            formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+
+            if let date = formatter.date(from: dateString) {
+                return date
+            }
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Format de date invalide: \(dateString)")
+        }
+
+        return try decoder.decode(T.self, from: data)
+    }
+}
